95d20163f0b24a071f240d5663952072e4aa2cf3
Saulo Dtarso <skdtarso@gmail.com>
2021-09-02 15:16:29 -0300

    Creating product migration

---
+++ database/migrations/2021_09_02_173406_create_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('pro_name');
+            $table->string('pro_sku')->unique();
+            $table->integer('pro_quantity');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
