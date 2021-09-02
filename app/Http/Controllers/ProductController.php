dead1e0b62ac42398f0c36ab00c64d2da0bac3c3
Saulo Dtarso <skdtarso@gmail.com>
2021-09-02 17:31:53 -0300

    Creating ProductController@create function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,4 +9,38 @@ use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller {
 
+    public function create(Request $request) {
+
+        try {
+
+            $ps = new ProductService;
+            $data = $request->all();
+            
+            $rules = $ps->getRulesCreate();
+            $validator = Validator::make($data, $rules);
+        
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'product' => $validator->errors()
+                ], 500);
+            }
+            
+            return response()->json([
+                'success' => true,
+                'product' => $ps->storeData($data)
+            ]);
+        } catch(Exception $e) {
+
+            return response()->json([
+                'success' => false,
+                'exception' => [
+                    'message' => $e->getMessage(),
+                    'code' => $e->getCode(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                ]
+            ], $e->getCode() ?? 500);
+        }
+    }
 }
