ddbd47448cf0a51fe270e7b0cbed8e1fd9eacc78
Saulo Dtarso <skdtarso@gmail.com>
2021-09-02 17:29:01 -0300

    Creating product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductService;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller {
+
+}
