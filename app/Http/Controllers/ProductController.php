0c1a15afaf35104e10f79e60ca95f5fe6e95caa5
Saulo Dtarso <skdtarso@gmail.com>
2021-09-02 21:54:04 -0300

    Creating ProductController@list method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,28 +9,55 @@ use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller {
 
+    public function list(Request $request)
+    {
+
+        try {
+
+            $ps = new ProductService;
+            $items = $ps->paginate($request->get('itemsPerPage') ?? 20);
+
+            return response()->json([
+                'success' => true,
+                'items' => $items
+            ]);
+        } catch (Exception $e) {
+
+            return response()->json([
+                'success' => false,
+                'exception' => [
+                    'message' => $e->getMessage(),
+                    'code' => $e->getCode(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                ]
+            ], $e->getCode() ?? 500);
+        }
+    }
+
+
     public function create(Request $request) {
 
         try {
 
             $ps = new ProductService;
             $data = $request->all();
-            
+
             $rules = $ps->getRulesCreate();
             $validator = Validator::make($data, $rules);
-        
+
             if ($validator->fails()) {
                 return response()->json([
                     'success' => false,
                     'product' => $validator->errors()
                 ], 500);
             }
-            
+
             return response()->json([
                 'success' => true,
                 'product' => $ps->storeData($data)
             ]);
-        } catch(Exception $e) {
+        } catch (Exception $e) {
 
             return response()->json([
                 'success' => false,
